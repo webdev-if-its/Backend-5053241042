0e6698cb442ae9d1205588d9dd616b1d577d49a4
ItsYad <ziyadraziq01@gmail.com>
2026-09-08 16:46:19 +0700

    ubah format CetakInfo di main

--- pertemuan-01/main.go
+++ pertemuan-01/main.go
@@ -27,9 +27,8 @@ func Sapa(nama string) string {
 // TODO(Level 5): gabungkan Nama, NRP, dan hasil runtime.Version() jadi satu
 // string siap cetak (lihat contoh format di SOAL.md).
 func CetakInfo(nama string) string {
-    return fmt.Sprintf("Nama: %s\nNRP: %s\n%s\n%s", nama, NRP, runtime.Version(), Sapa(nama))
+    return fmt.Sprintf("Nama: %s\nNRP: %s\nGo=%s\n%s", nama, NRP, runtime.Version(), Sapa(nama))
 }
-
 
 func main() {
     nama := ResolveNama(os.Args[1:], NRP)
